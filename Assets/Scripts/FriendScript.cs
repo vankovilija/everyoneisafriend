96289d89329cfd67a1b6b8ef54a9f0d7f137ba29
Mario Ampov <ampov.mario@gmail.com>
2014-01-18 22:39:45 +0100

    Friend angry mode work

--- Assets/Scripts/FriendScript.cs
+++ Assets/Scripts/FriendScript.cs
@@ -3,14 +3,15 @@ using System.Collections;
 
 public class FriendScript : MonoBehaviour {
 
-	public Texture2D happyFace;
-	public Texture2D angryFace;
+	public Sprite happyFace;
+	public Sprite angryFace;
 	
 	public FoodType[] foodTypes;
 
 	public float happyMovementSpeed;
 	public float angryMovementSpeed;
 	public float veryAngryMovementSpeed;
+
 	public float veryAngryTime;
 
 	private enum FriendState {
@@ -23,18 +24,68 @@ public class FriendScript : MonoBehaviour {
 	private FriendState state;
 	private float timeInState;
 
+	private SpriteRenderer render;
+	private FriendMovementScript movement;
+
 	// Use this for initialization
 	void Start () {
+		render = GetComponent<SpriteRenderer> ();
 		state = FriendState.angry;
+		movement = GetComponent<FriendMovementScript> ();
+		currentFoodType = foodTypes[Random.Range(0, foodTypes.Length)];
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+
+		timeInState += Time.deltaTime;
+		if (state == FriendState.veryAngry && timeInState > veryAngryTime) {
+			goAngry();
+		}
+
+	}
+
+	void goHappy() {
+
+		if (state == FriendState.angry) {
+			state = FriendState.happy;
+			movement.moveSpeed = happyMovementSpeed;
+			render.sprite = happyFace;
+			timeInState = 0;
+		}
+
+	}
+
+	void goVeryAngry() {
+
+		if (state == FriendState.angry) {
+			state = FriendState.veryAngry;
+			movement.moveSpeed = veryAngryMovementSpeed;
+			render.sprite = happyFace;
+			timeInState = 0;
+		}
+
+	}
+
+	void goAngry() {
+
+		if (state == FriendState.veryAngry) {
+			state = FriendState.angry;
+			movement.moveSpeed = angryMovementSpeed;
+			render.sprite = happyFace;
+			timeInState = 0;
+		}
 	}
 
 	void OnCollisionEnter2D(Collision2D coll) {
-
+		if (coll.gameObject.tag == "Bullet") {
+			FoodType bulletType = coll.gameObject.GetComponent<Bullet> ().type;
+			if (bulletType == currentFoodType) {
+				goHappy();
+			} else {
+				goVeryAngry();
+			}
+		}
 	}
 	
 	
