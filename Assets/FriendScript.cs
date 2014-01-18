087c5372ee6001e69492e309c30ef8f297d91803
Mario Ampov <ampov.mario@gmail.com>
2014-01-18 22:06:43 +0100

    Friend hit work

--- Assets/FriendScript.cs
+++ Assets/FriendScript.cs
@@ -6,15 +6,36 @@ public class FriendScript : MonoBehaviour {
 	public Texture2D happyFace;
 	public Texture2D angryFace;
 	
-	public FoodType foodType;
+	public FoodType[] foodTypes;
+
+	public float happyMovementSpeed;
+	public float angryMovementSpeed;
+	public float veryAngryMovementSpeed;
+	public float veryAngryTime;
+
+	private enum FriendState {
+		happy,
+		angry,
+		veryAngry
+	};
+
+	private FoodType currentFoodType;
+	private FriendState state;
+	private float timeInState;
 
 	// Use this for initialization
 	void Start () {
-
+		state = FriendState.angry;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+
+	void OnCollisionEnter2D(Collision2D coll) {
+
+	}
+	
+	
 }
