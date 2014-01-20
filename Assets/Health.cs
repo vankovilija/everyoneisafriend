e3afc4586d6cf3272e530dfdcb31e407c7ae118a
Ilija Vankov <ilija@peakgames.net>
2014-01-20 22:33:41 +0100

    add death script and triggers

--- Assets/Health.cs
+++ Assets/Health.cs
@@ -2,17 +2,6 @@
 using System.Collections;
 
 public class Health : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
-
 	void die() {
 		transform.position = GetComponent<PlayerControl> ().spawnPosition;
 	}
