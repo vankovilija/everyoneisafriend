2524454ad99b1162f673fceeea530f9ff0ab20b3
Ilija Vankov <ilija@peakgames.net>
2014-01-21 17:22:27 +0100

    latest work

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -4,5 +4,6 @@ using System.Collections;
 public class Health : MonoBehaviour {
 	void die() {
 		transform.position = GetComponent<PlayerControl> ().spawnPosition;
+		rigidbody2D.velocity = new Vector2 (0f, 0f);
 	}
 }
