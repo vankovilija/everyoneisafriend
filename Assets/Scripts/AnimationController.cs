d69e19e631c2d77591fbdf2c283f085f1a8ff44d
Irena_Ljubik <irena_ljubik@hotmail.com>
2014-01-22 13:36:17 +0100

    Run animation redo

---
+++ Assets/Scripts/AnimationController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnimationController : MonoBehaviour {
+
+	public Animator anim;
+
+	// Use this for initialization
+	void Start () {
+
+		anim = GetComponentInChildren<Animator> ();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		anim.SetFloat("Speed", rigidbody2D.velocity.x);
+	}
+}
