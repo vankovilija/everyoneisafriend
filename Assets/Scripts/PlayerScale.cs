520955347f86258158308c22d06efc5e7af9c31e
Mario Ampov <ampov.mario@gmail.com>
2014-01-24 21:54:34 +0100

    Limited Time Component base class

---
+++ Assets/Scripts/PlayerScale.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerScale : LimitedTimeComponent {
+
+	public PlayerScale(float timeLimit, MonoBehaviour restoreScript, float scale, float mass)
+		:base(timeLimit, restoreScript) {
+
+	}
+
+
+
+}
