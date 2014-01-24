39ec69a0ce66f18254f24074f21a4cd98615ee8e
Irena_Ljubik <irena_ljubik@hotmail.com>
2014-01-24 22:19:40 +0100

    Jump and Fly added

---
+++ Assets/Scripts/Control/Fly.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Fly : MonoBehaviour {
+
+	private PlayerControl player;
+	private bool fly = false;
+
+	public float flySpeed;
+	public float fallSpeed;
+
+	public Fly(){
+
+		flySpeed = 2f;
+		fallSpeed = -0.3f;
+	}
+
+	// Use this for initialization
+	void Start () {
+		player = GetComponent<PlayerControl> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void FixedUpdate(){
+
+		fly = Input.GetButton ("Jump");
+
+		if (fly) {
+		
+			rigidbody2D.velocity = new Vector2(rigidbody2D.velocity.x, flySpeed);
+
+		}else if(!player.grounded){
+
+			rigidbody2D.velocity = new Vector2(rigidbody2D.velocity.x, fallSpeed);
+		}
+
+	}
+}
---
+++ Assets/Scripts/Control/Jump.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Jump : MonoBehaviour {
+
+	private PlayerControl player;
+	private bool jump = false;
+
+	public float jumpForce = 12;
+
+	// Use this for initialization
+	void Start () {
+
+		player = GetComponent<PlayerControl> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetButtonDown ("Jump") && player.grounded) {
+			jump = true;
+		}
+	}
+
+	void FixedUpdate(){
+
+		if (jump) {
+
+			GetComponent<AudioSource>().Play();
+
+			rigidbody2D.velocity = new Vector2(rigidbody2D.velocity.x, rigidbody2D.velocity.y + jumpForce);
+
+			jump = false;
+		}
+	}
+}
