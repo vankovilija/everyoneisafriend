c0347fafd3e04af631f95f8a802505f5818faa36
Mario Ampov <ampov.mario@gmail.com>
2014-01-26 00:42:11 +0100

    Kill big player

---
+++ Assets/Scripts/KillPlayerWithComponent.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KillPlayerWithComponent : MonoBehaviour {
+
+	public string deathTag = "Player";
+	public Object killComponent;
+	
+	void OnTriggerEnter2D (Collider2D col) {
+
+		KillObject (col.gameObject);
+
+	}
+
+	void OnTriggerStay2D (Collider2D col) {
+
+		KillObject (col.gameObject);
+
+	}
+	
+	void KillObject(GameObject obj){
+		if (deathTag == obj.tag) {
+			MonoBehaviour component = obj.GetComponent (killComponent.name) as MonoBehaviour;
+			if (component && component.enabled) {
+				//kill the object
+				obj.SendMessage("die");
+			}			
+		}
+	}
+
+}
