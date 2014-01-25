9e6e81c9f4c050f0340418dcd4e7a8662dfaf2a6
Ilija Vankov <ilija@peakgames.net>
2014-01-25 16:48:22 +0100

    dialog show ray cast change

--- Assets/Scripts/DialogNPC.cs
+++ Assets/Scripts/DialogNPC.cs
@@ -31,11 +31,32 @@ public class DialogNPC : MonoBehaviour {
 		rayPoint.x += sign * coliderWidth;
 		RaycastHit2D hitToPlayer = Physics2D.Raycast(rayPoint, direction);
 
+		BoxCollider2D npcColider = GetComponent<BoxCollider2D> ();
+		BoxCollider2D playerColider = player.GetComponent<BoxCollider2D> ();
+
 		if(hitToPlayer.collider.tag != PlayerControl.PLAYER_TAG){
-			GetComponent<NPCMovementScript> ().ContinueWalking ();
-			if(hasDialog){
-				dialogsInstance.removeDialog();
-				hasDialog = false;
+			Rect NPCBound = new Rect (transform.position.x + npcColider.center.x - (npcColider.size.x * 0.5f), transform.position.y + npcColider.center.y - (npcColider.size.y * 0.5f),
+			                          npcColider.size.x, npcColider.size.y);
+			
+			Rect PLAYERBounds = new Rect (player.transform.position.x + playerColider.center.x - (playerColider.size.x * 0.5f), player.transform.position.y + playerColider.center.y - (playerColider.size.y * 0.5f),
+			                              playerColider.size.x, playerColider.size.y);
+			
+			if(NPCBound.Overlaps(PLAYERBounds)){
+				GetComponent<NPCMovementScript> ().StopWalking ();
+				Debug.Log("stop");
+				if(dialogsInstance != null){
+					Vector3 spawnPosition = transform.position;
+					spawnPosition.x += dialogOffset.x;
+					spawnPosition.y += (npcColider.size.y * 0.5f) + dialogOffset.y;
+					dialogsInstance.showDialogAt(spawnPosition, NPCPowerUp);
+					hasDialog = true;
+				}
+			}else{
+				GetComponent<NPCMovementScript> ().ContinueWalking ();
+				if(hasDialog){
+					dialogsInstance.removeDialog();
+					hasDialog = false;
+				}
 			}
 			return;
 		}
@@ -47,22 +68,69 @@ public class DialogNPC : MonoBehaviour {
 		rayPoint.x += sign * coliderWidth;
 		RaycastHit2D hitToNPC = Physics2D.Raycast(rayPoint, direction);
 
+		if (hitToNPC.collider.gameObject != gameObject) {
+			Rect NPCBound = new Rect (transform.position.x + npcColider.center.x - (npcColider.size.x * 0.5f), transform.position.y + npcColider.center.y - (npcColider.size.y * 0.5f),
+			                          npcColider.size.x, npcColider.size.y);
+			
+			Rect PLAYERBounds = new Rect (player.transform.position.x + playerColider.center.x - (playerColider.size.x * 0.5f), player.transform.position.y + playerColider.center.y - (playerColider.size.y * 0.5f),
+			                              playerColider.size.x, playerColider.size.y);
+			
+			if(NPCBound.Overlaps(PLAYERBounds)){
+				GetComponent<NPCMovementScript> ().StopWalking ();
+				Debug.Log("stop");
+				if(dialogsInstance != null){
+					Vector3 spawnPosition = transform.position;
+					spawnPosition.x += dialogOffset.x;
+					spawnPosition.y += (npcColider.size.y * 0.5f) + dialogOffset.y;
+					dialogsInstance.showDialogAt(spawnPosition, NPCPowerUp);
+					hasDialog = true;
+				}
+			}else{
+				GetComponent<NPCMovementScript> ().ContinueWalking ();
+				if(hasDialog){
+					dialogsInstance.removeDialog();
+					hasDialog = false;
+				}
+			}
+			return;
+		}
+
+
 		float distance = Vector3.Distance (hitToPlayer.point, hitToNPC.point);
 
 		if (distance < stopDistance) {
 			GetComponent<NPCMovementScript> ().StopWalking ();
+			Debug.Log("stop");
 			if(dialogsInstance != null){
 				Vector3 spawnPosition = transform.position;
 				spawnPosition.x += dialogOffset.x;
-				spawnPosition.y += (GetComponent<BoxCollider2D>().size.y * 0.5f) + dialogOffset.y;
+				spawnPosition.y += (npcColider.size.y * 0.5f) + dialogOffset.y;
 				dialogsInstance.showDialogAt(spawnPosition, NPCPowerUp);
 				hasDialog = true;
 			}
 		} else {
-			GetComponent<NPCMovementScript> ().ContinueWalking ();
-			if(hasDialog){
-				dialogsInstance.removeDialog();
-				hasDialog = false;
+			Rect NPCBound = new Rect (transform.position.x + npcColider.center.x - (npcColider.size.x * 0.5f), transform.position.y + npcColider.center.y - (npcColider.size.y * 0.5f),
+			                          npcColider.size.x, npcColider.size.y);
+			
+			Rect PLAYERBounds = new Rect (player.transform.position.x + playerColider.center.x - (playerColider.size.x * 0.5f), player.transform.position.y + playerColider.center.y - (playerColider.size.y * 0.5f),
+			                              playerColider.size.x, playerColider.size.y);
+			
+			if(NPCBound.Overlaps(PLAYERBounds)){
+				GetComponent<NPCMovementScript> ().StopWalking ();
+				Debug.Log("stop");
+				if(dialogsInstance != null){
+					Vector3 spawnPosition = transform.position;
+					spawnPosition.x += dialogOffset.x;
+					spawnPosition.y += (npcColider.size.y * 0.5f) + dialogOffset.y;
+					dialogsInstance.showDialogAt(spawnPosition, NPCPowerUp);
+					hasDialog = true;
+				}
+			}else{
+				GetComponent<NPCMovementScript> ().ContinueWalking ();
+				if(hasDialog){
+					dialogsInstance.removeDialog();
+					hasDialog = false;
+				}
 			}
 		}
 
